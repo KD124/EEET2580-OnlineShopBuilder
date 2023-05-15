329ed37b909f4d2ee10aa02671f0aca86f97ee1a
KD124 <s3836280@rmit.edu.vn>
2023-05-15 16:19:54 +0700

    build navigation bar

---
+++ js/dashboard.js
@@ -0,0 +1,26 @@
+const nav_bar = document.querySelector(".navbar");
+const menu_btn = document.querySelector(".menu_btn");
+const page_content = document.querySelector(".container");
+
+
+function openMenu() {
+    nav_bar.style.width = "200px";
+    menu_btn.style.width = "0";
+    page_content.style.marginLeft = "200px"
+    document.body.style.background = "rgba(0,0,0,0.4)";
+}
+
+function closeMenu() {
+    nav_bar.removeAttribute("style");
+    menu_btn.removeAttribute("style");
+    page_content.removeAttribute("style");
+    document.body.removeAttribute("style");
+}
+
+
+window.addEventListener('resize', function (event) {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+        // Viewport is less or equal to 700 pixels wide
+        closeMenu();
+    }
+}, true);
